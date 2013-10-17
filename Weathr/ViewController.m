eaddbcbe7587d5c79f011fe401c024ff36a7fe68
Paul Williamson <squarefrog@gmail.com>
2013-10-17 18:09:48 +0100

    Added Core Location boilerplate to View Controller

--- Weathr/ViewController.m
+++ Weathr/ViewController.m
@@ -7,12 +7,14 @@
 //
 
 #import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController ()
+@interface ViewController () <CLLocationManagerDelegate>
 
 @property (nonatomic, weak) IBOutlet UIImageView *weatherIcon;
 @property (nonatomic, weak) IBOutlet UILabel *weatherDescription;
 @property (nonatomic, weak) IBOutlet UILabel *lastUpdatedLabel;
+@property (nonatomic, strong) CLLocationManager *locationManager;
 
 @end
 
@@ -21,13 +23,41 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Fetch current location
+    _locationManager = [[CLLocationManager alloc] init];
+    _locationManager.delegate = self;
+    [_locationManager startUpdatingLocation];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Core Location
+
+- (void)locationManager:(CLLocationManager *)manager
+    didUpdateToLocation:(CLLocation *)newLocation
+           fromLocation:(CLLocation *)oldLocation
+{
+    [_locationManager stopUpdatingLocation];
+    CLGeocoder *geocoder = [[CLGeocoder alloc] init];
+    [geocoder reverseGeocodeLocation:newLocation completionHandler:^(NSArray *placemarks, NSError *error) {
+        NSLog(@"%@\nlocation: %@", placemarks[0], newLocation);
+    }];
+}
+
+- (void)locationManager:(CLLocationManager *)manager
+       didFailWithError:(NSError *)error
+{
+    [_locationManager stopUpdatingLocation];
+    [[[UIAlertView alloc] initWithTitle:@"Error fetching location"
+                                message:@"Please ensure you have enabled location services"
+                               delegate:nil
+                      cancelButtonTitle:@"Ok"
+                       otherButtonTitles:nil] show];
 }
 
 @end
