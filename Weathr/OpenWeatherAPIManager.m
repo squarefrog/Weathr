fa9f149dd79adc1d4ade2e2b954e338e703fb941
Paul Williamson <squarefrog@gmail.com>
2013-10-20 14:32:22 +0100

    Created UNTESTED data download method

--- Weathr/OpenWeatherAPIManager.m
+++ Weathr/OpenWeatherAPIManager.m
@@ -30,6 +30,27 @@ NSString * const OpenWeatherMapAPIUrl = @"http://api.openweathermap.org/data/2.5
     fetchingURL = [NSURL URLWithString:newUrl];
 }
 
+#pragma mark - Data transfer
+// I'm not entirely sure the best way to test this at the moment.
+// I think it could possibly be tested using a mock NSURLSession
+// and NSURLSessionTask
+// TODO: Create mock object to test this
+- (void)fetchWeatherData
+{
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLRequest *request = [NSURLRequest requestWithURL:fetchingURL];
+    __weak OpenWeatherAPIManager *weakself = self;
+    NSURLSessionTask *task = [session dataTaskWithRequest:request
+                                        completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                            NSHTTPURLResponse *r = (NSHTTPURLResponse*)response;
+                                            if (r.statusCode == 200)
+                                                [weakself tellDelegateDataTaskSucceededWithData:data];
+                                            else
+                                                [weakself tellDelegateDataTaskFailedWithHTTPURLResponse:r];
+                                        }];
+    [task resume];
+}
+
 #pragma mark - Delegation
 - (void)tellDelegateDataTaskSucceededWithData:(NSData *)data
 {
